a8b33d1deebc4293cf9a40aa3472f571044606b0
bhushan2026 <bhushandeshpande0309@gmail.com>
2025-07-15 16:01:54 +0530

    file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
 import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home.jsx'
+import Doctors from './pages/Doctors.jsx'
+import Login from './pages/Login.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+// import Footer from './components/Footer.jsx'
+// import Receipt from './pages/Receipt.jsx'
 
 function App() {
 
   return (
     <>
-        <h1>Hello world!!</h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        {/* <Route path='/about' element={<About />} /> */}
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        {/* <Route path='/receipt/:id' element={<Receipt/>}/> */}
+      </Routes>
     </>
   )
 }
